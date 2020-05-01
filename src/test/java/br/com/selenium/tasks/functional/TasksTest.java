9fca3612482532de95eaee50c02ef96569b50240
Rafael Alberto <rafael.alberto1703@gmail.com>
2020-05-01 10:23:32 -0300

    fixing due dates

--- src/test/java/br/com/selenium/tasks/functional/TasksTest.java
+++ src/test/java/br/com/selenium/tasks/functional/TasksTest.java
@@ -72,7 +72,7 @@ public class TasksTest {
             webDriver.findElement(By.id(ADD_TODO_BUTTON)).click();
 
             webDriver.findElement(By.id(TASK_FIELD)).sendKeys("Study Selenium");
-            webDriver.findElement(By.id(DUE_DATE_FIELD)).sendKeys("01/01/2025");
+            webDriver.findElement(By.id(DUE_DATE_FIELD)).sendKeys("01/01/2020");
             webDriver.findElement(By.id(SAVE_BUTTON)).click();
 
             String message = webDriver.findElement(By.id(MESSAGE_LABEL)).getText();
